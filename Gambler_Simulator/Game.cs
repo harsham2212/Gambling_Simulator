4a7c90a5136dd502afdd7751648e6179ab678f69
Harsh Singh <kmrharsh50@gmail.com>
2021-12-22 10:44:08 +0530

    [Harsh]Added:Stake  in every game Gambling_simulator feature implemented

---
+++ Gambler_Simulator/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gambler_Simulator
+{
+    public class Game
+    {
+        //int stake = 100;
+        //int stake_amount=0;
+
+       public static void game_play()
+        {
+            Console.WriteLine("Every Day Gambler Stake of $100");
+            Console.WriteLine("Every Game gambler bets of $1");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Gambler_Simulator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Gambler_Simulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Gambling Simulator Problem!\n");
+            Game.game_play();
+        }
+    }
+}
